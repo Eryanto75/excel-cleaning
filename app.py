193f84a08b1854d64d5ab9e04c9f9a02921275f3
Ery <143410318+Eryanto75@users.noreply.github.com>
2025-01-09 15:58:14 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 import streamlit as st
 import pandas as pd
+from io import BytesIO
 
 st.title("Data Cleaning App")
 
@@ -36,14 +37,21 @@ if uploaded_file is not None:
         st.write("Cleaned Data:")
         st.write(df_cleaned)
 
+        # Fungsi untuk mengubah DataFrame menjadi file Excel dalam memori
+        def to_excel(df):
+            output = BytesIO()
+            with pd.ExcelWriter(output, engine='openpyxl') as writer:
+                df.to_excel(writer, index=False, sheet_name='Cleaned Data')
+            return output.getvalue()
+
         # Tombol untuk mendownload data yang telah dibersihkan
         file_extension = 'csv' if uploaded_file.name.endswith('.csv') else 'xlsx'
+        
         if file_extension == 'csv':
             cleaned_file = df_cleaned.to_csv(index=False).encode('utf-8')
             mime_type = 'text/csv'
         else:
-            # Menggunakan engine default tanpa menyebutkan engine
-            cleaned_file = df_cleaned.to_excel(index=False)  # Menggunakan openpyxl secara default
+            cleaned_file = to_excel(df_cleaned)
             mime_type = 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet'
 
         st.download_button(
